0e5cf3c014ae611514f200a920fc352e1c8a0b34
Mario Amelung <mario.amelung@gmx.de>
2005-12-21 14:17:58 +0000

    removed fields 'source' and 'description' from schema; matching modifications in templates

--- skins/ECAssignmentBox/assignment_add.py
+++ skins/ECAssignmentBox/assignment_add.py
@@ -13,17 +13,11 @@ from StringIO import StringIO
 # resourcestrings
 I18N_DOMAIN = 'eduComponents'
 
-EC_ASSIGNMENT_BOX_META_TYPE = 'ECAssignmentBox'
-EC_ASSIGNMENT_BOX_QC_META_TYPE = 'ECAssignmentBoxQC'
-
 REQUEST  = container.REQUEST
 RESPONSE = REQUEST.RESPONSE
 
 # set default target action (e.g. in case of an error)
 target_action = context.getTypeInfo().getActionById('view')
-
-# remember the context type
-contextType = context.meta_type
 
 # was the user logged in?
 if memberId == '':
@@ -65,11 +59,6 @@ if msg == '':
               msgid   = 'file_read_error',\
               domain  = I18N_DOMAIN,\
               default = 'No text inserted and uploaded file could not be read.')
-    else:
-       # put file's content into source only for submissions 
-       # to ECAssignmentBoxQC-objects
-       if contextType == EC_ASSIGNMENT_BOX_QC_META_TYPE:
-          source = sstr
 
 # no erros until now, process the submission
 if msg == '':
@@ -79,13 +68,9 @@ if msg == '':
     id = str(memberId) + '.' + now.strftime('%Y%m%d') + '.' + now.strftime('%H%M%S')
 
     # create the assignment object
-    #context.invokeFactory(id=id, type_name=context.allowed_content_types[0])
     context.invokeFactory(context.allowed_content_types[0], id)
     qca = getattr(context, id)
 
-    qca.setField('source', source)
-    #qca.setField('user_id', memberId)
-    qca.setField('datetime', now)
     qca.setField('file', file)
 
     filename = qca.getFilename(key='file')
@@ -97,40 +82,11 @@ if msg == '':
 
     qca.setFilename(filename, key='file')
     
-    # qca.editMetadata(title=id)
-    # qca.editMetadata(language='') # '' means "language-neutral"
-
-    # evaluate only in case of ECAssignmentQC
-    if contextType == EC_ASSIGNMENT_BOX_QC_META_TYPE:
-        result = qca.evaluate(
-            context.getModel_solution(), 
-            context.getQc_property(),
-            context.getSpoolerConnection(),
-            context.getCheckerBackend())
-        
-        # set next workflow state automatic
-        # FIXME: This doesn't work
-        #wtool = context.portal_workflow
-        #wtool.doActionFor(qca, 'review')
-
-    else:
-        result = None
-
-    if not result:
-        # The submission was evaluated.
-        msg = context.translate(\
-            msgid   = 'submission_success_evaluation_success',\
-            domain  = I18N_DOMAIN,\
-            default = 'Your submission has been saved.')
-    else:
-        msg = result
-        # The submission couldn't be evaluated.
-        #msg = context.translate(\
-        #    msgid   = 'submission_success_evaluation_failed',\
-        #    domain  = I18N_DOMAIN,\
-        #    default = 'Your solution has been submitted but could not be evaluated.')
-            
-        # TODO: send email to teacher
+    # The submission was sucessfully saved as assignment object.
+    msg = context.translate(\
+        msgid   = 'submission_success',\
+        domain  = I18N_DOMAIN,\
+        default = 'Your submission has been saved.')
 
     target_action = '%s/%s' % (qca.getId(), qca.getTypeInfo().getActionById('view'))
 

f55e71f16f2ead817b47b91009cd288568809756
Mario Amelung <mario.amelung@gmx.de>
2006-01-11 08:30:34 +0000

    removed

--- make_i18n_pot.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem python C:\Programme\Python23\Scripts\i18ndude rebuild-pot --pot i18n\eduComponent.pot --create eduComponents -s .
-python C:\Programme\Python23\Scripts\i18ndude rebuild-pot --pot i18n\eduComponents.pot --create eduComponents --merge i18n\eduComponents.pot -s .
- 
-pause

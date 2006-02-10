a682d779e864cb13dddb1d0f0b3628da6ca61312
Mario Amelung <mario.amelung@gmx.de>
2006-02-10 08:10:48 +0000

    Added license information.

--- validators.py
+++ validators.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 # $Id$
 #
-# Copyright (c) 2005 Otto-von-Guericke-Universit‰t Magdeburg
+# Copyright (c) 2006 Otto-von-Guericke-Universit√§t Magdeburg
 #
 # This file is part of ECAssignmentBox.
+#
+# ECAssignmentBox is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# ECAssignmentBox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ECAssignmentBox; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 try:
     from Products.validation.interfaces.IValidator import IValidator
@@ -13,7 +27,10 @@ except ImportError:
     from interfaces.IValidator import IValidator
     del sys, os
 
+
 class PositiveNumberValidator:
+    """
+    """
     __implements__ = IValidator
 
     def __init__(self, name, title='', description=''):

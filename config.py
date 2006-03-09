f25431b0803dd6658e1ce2d508947c588651b0ab
Michael Piotriowski <mxp@dynalabs.de>
2006-03-09 14:10:51 +0000

    Changed CMFCorePermissions (deprecated) to CMFCore.permissions.

--- config.py
+++ config.py
@@ -19,7 +19,7 @@
 # along with ECAssignmentBox; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-from Products.CMFCore.CMFCorePermissions import setDefaultRoles, AddPortalContent
+from Products.CMFCore.permissions import setDefaultRoles, AddPortalContent
 from Products.Archetypes.public import DisplayList
 
 GLOBALS = globals()

67a6a59811b3f5b90c7ef357e203b6a2f4aad7b7
Mario Amelung <mario.amelung@gmx.de>
2011-08-14 09:52:40 +0000

    Copied main development sources from branch_devel_plone-4 to trunk.

---
+++ Products/ECAssignmentBox/content/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# $Id$
+#
+# Copyright (c) 2006-2011 Otto-von-Guericke-UniversitŠt Magdeburg
+#
+# This file is part of ECAssignmentBox.
+#
+import ECFolder
+import ECAssignmentBox
+import ECAssignment
+
---
+++ Products/ECAssignmentBox/tool/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# $Id$
+#
+# Copyright (c) 2006-2011 Otto-von-Guericke-UniversitŠt Magdeburg
+#
+# This file is part of ECAssignmentBox.
+#
+import ECABTool
